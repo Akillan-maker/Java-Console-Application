a9d9e412a598bfe18b67abca0cc6f363272a32c8
Akil <akilhunter29@gmail.com>
2025-01-08 15:03:23 +0530

    Delete ATM/FiveHundred.java

--- ATM/FiveHundred.java
+++
@@ -1,9 +0,0 @@
-package ATM;
-
-public class FiveHundred extends Notes{
-
-    public FiveHundred(String note, int count) {       // Constructor is created and initialized
-
-        super(note,count);     // Assigns note and count from super class
-    }
-}
